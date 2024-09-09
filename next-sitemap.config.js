72f88eebdf6b349073e6963314aafdc8f80e87f3
krishna1584 <abhi19ay@gmail.com>
2024-09-09 16:00:25 +0530

    Update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://mooncoded.vercel.app",
+  siteUrl: process.env.SITE_URL || "",
 };
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -28,20 +28,20 @@ function MapComp() {
         <Map center={[-19.740235, -47.894058]} zoom={15} zoomControl={false}>
           <ContactInfo>
             <div>
-              <p>Vitor Moon,</p>
-              <p>Uberaba, MG. Brazil</p>
+              <p>Krishna</p>
+              <p>Chandigarh</p>
             </div>
             <div>
               <span>
-                <AiFillPhone /> +55 (34) 9 96484068
+                <AiFillPhone />
               </span>
               <span>
-                <MdEmail className="email" /> mooncoded.bs@gmail.com
+                <MdEmail className="email" />
               </span>
             </div>
           </ContactInfo>
           <TileLayer
-            url={`https://api.mapbox.com/styles/v1/twilightmoon/cl70w18v6000v15moe6rcn2iy/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidHdpbGlnaHRtb29uIiwiYSI6ImNsOTFmcWpjZjFhMTEzeG5uMWl5MXl5MWgifQ.D8WHcu-sz_JmbKyFBOCYAw`}
+            
           />
           <Marker icon={MarkerIcon} position={[-19.740235, -47.894058]}>
             <Popup>
@@ -53,15 +53,15 @@ function MapComp() {
         <Map center={[-19.740235, -47.894058]} zoom={15} zoomControl={false}>
           <ContactInfo>
             <div>
-              <p>Vitor Moon,</p>
-              <p>Uberaba, MG. Brazil</p>
+              <p>Krishna</p>
+              <p></p>
             </div>
             <div>
               <span>
-                <AiFillPhone /> +55 (34) 9 96484068
+                <AiFillPhone />
               </span>
               <span>
-                <MdEmail className="email" /> mooncoded.bs@gmail.com
+                <MdEmail className="email" />
               </span>
             </div>
           </ContactInfo>
